c37fb066374b44a2906ab707953a5f5068642fd0
sumidiot <nick.hamblet@gmail.com>
2020-02-11 22:04:20 -0500

    Add/refine a few more types to close out chapter 5

--- src/main/scala/sumidiot/dmmf/OrderTaking.scala
+++ src/main/scala/sumidiot/dmmf/OrderTaking.scala
@@ -1,5 +1,7 @@
 package sumidiot.dmmf
 
+import scala.concurrent.Future
+
 
 /**
  * This object represents the beginnings of the implemented version of the domain
@@ -8,27 +10,40 @@ package sumidiot.dmmf
 object OrderTaking {
 
   sealed trait ProductCode extends Any
-  case class WidgetCode(code: String) extends AnyVal with ProductCode
-  case class GizmoCode(code: String) extends AnyVal with ProductCode
+  case class WidgetCode(code: String) extends AnyVal with ProductCode // will add constraint later
+  case class GizmoCode(code: String) extends AnyVal with ProductCode  // will add constraint later
 
   sealed trait OrderQuantity extends Any
   case class UnitQuantity(quantity: Int) extends AnyVal with OrderQuantity
   case class KilogramQuantity(quantity: Double) extends AnyVal with OrderQuantity
 
-  type CustomerId = Int
-  type OrderId = Int
+  // placeholder types for ids of "entity" types
+  type OrderId = Void
+  type OrderLineId = Void
+  type CustomerId = Void
 
   // placeholder types, not sure what a better way to capture these might be
   type CustomerInfo = Void
   type ShippingAddress = Void
   type BillingAddress = Void
-  type OrderLine = Void
+  type Price = Void
+  type BillingAmount = Void
+
+  case class OrderLine(
+    id: OrderLineId,
+    orderId: OrderId,
+    productCode: ProductCode,
+    orderQuantity: OrderQuantity,
+    price: Price
+  )
 
   case class UnvalidatedOrder(
-    customerInfo: CustomerInfo,
+    id: OrderId,
+    customerId: CustomerId, // aggregate entities should store references to contained entities
     shippingAddress: ShippingAddress,
     billingAddress: BillingAddress,
-    orderLines: List[OrderLine]
+    orderLines: List[OrderLine],
+    amountToBill: BillingAmount
   )
 
   // more placeholder types
@@ -42,7 +57,7 @@ object OrderTaking {
     errorDescription: String
   )
 
-  type ValidationoResponse[R] = Future[Either[List[ValidationError], R]]
+  type ValidationResponse[R] = Future[Either[List[ValidationError], R]]
   type ValidateOrder = UnvalidatedOrder => ValidationResponse[ValidatedOrder]
 
   case class PlaceOrderEvents(
@@ -51,6 +66,8 @@ object OrderTaking {
     billableOrderPlaced: BillableOrderPlaced
   )
 
-  type PlaceOrder = UnvalidatedOrder => PlaceOrderEvents
+  sealed trait PlaceOrderError
+  case class OrderValidationErrors(validationErrors: List[ValidationError])
+  type PlaceOrder = UnvalidatedOrder => Either[PlaceOrderError, PlaceOrderEvents]
     
 }
